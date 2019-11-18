581ec52a4da894d7f2ba15a59c3f644da1a5d3b0
jsm <jsmammen@gmail.com>
2019-11-18 22:52:54 +0530

    Allows json input without key property

--- src/main/java/kafka/streams/globalktable/join/KafkaStreamsGlobalKTable.java
+++ src/main/java/kafka/streams/globalktable/join/KafkaStreamsGlobalKTable.java
@@ -2,7 +2,9 @@ package kafka.streams.globalktable.join;
 
 import java.util.function.Function;
 
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.state.KeyValueIterator;
@@ -13,6 +15,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,19 +34,11 @@ public class KafkaStreamsGlobalKTable {
     @RestController
     @RequestMapping("user")
     public static class KStreamToTableJoinApplication {
-
         @Bean
         public Function<KStream<String, User>, KStream<String, User>> process() {
-            // final JsonSerde<User> userSerde = new JsonSerde<>();
-            /*
-             * return input -> input.map((key, user) -> new KeyValue<>(user.getId(), user))
-             * .groupByKey(Grouped.with(Serdes.String(), userSerde)) .reduce((aggValue,
-             * newValue) -> newValue, Materialized.as("allusers")).toStream();
-             */
-
-            return input -> input.groupByKey().reduce((aggValue, newValue) -> newValue, Materialized.as("allusers"))
-                    .toStream();
-
+             return input -> input.map((key, user) -> new KeyValue<String, User>(user.getId(), user))
+             .groupByKey(Grouped.with(Serdes.String(), new JsonSerde<>(User.class)))
+             .reduce((aggValue,newValue) -> newValue, Materialized.as("allusers")).toStream();
         }
 
         @Autowired
@@ -68,7 +63,7 @@ public class KafkaStreamsGlobalKTable {
         }
 
         @Scheduled(fixedRate = 5000, initialDelay = 5000)
-        public void printProductCounts() {
+        public void printUserTable() {
             if (userStore == null) {
                 userStore = interactiveQueryService.getQueryableStore("allusers",
                         QueryableStoreTypes.<String, User>keyValueStore());
